e66269fb7cfc6b323476e78b91c3c760bc5ddf58
yugandhar009 <yugandhar060709@gmail.com>
2026-01-20 16:36:44 +0530

    Add files via upload

---
+++ LAB/l1p2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class l1p2{
+	public static void main(String[] args){
+		double x=50.0;
+		int d=(int)x;
+		
+			System.out.println(d);
+	}
+}
+
---
+++ LAB/l1p3.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class l1p3{
+	public static void main(String[] args){
+		int x=12;
+		double d=50.0;
+		char a='n';
+		boolean s=true;
+		float b=20.32f;
+		
+			System.out.println("integer:"+x+"\ndouble:"+d+ "\nchar:"+a+"\nboolean:"+s+"\nfloat:"+b);
+			
+			}
+	} 
+
